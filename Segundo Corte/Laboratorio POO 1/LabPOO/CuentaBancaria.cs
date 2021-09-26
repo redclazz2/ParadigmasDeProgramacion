95f728246efd6ec94282d49add62a11b9c248b05
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2021-09-26 00:35:33 -0500

    Lab2

---
+++ Segundo Corte/Laboratorio POO 1/LabPOO/CuentaBancaria.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LabPOO
+{
+    public class CuentaBancaria
+    {
+        //Atributos
+        private int numeroCuenta;
+        private float saldo;
+        private string nombreTitular, tipoCuenta;
+        
+        //Definición de Get y Set 
+        public string NombreTitular
+        {
+            get { return nombreTitular; }
+            set { nombreTitular = value; }
+        }
+
+        public string TipoCuenta
+        {
+            get { return tipoCuenta; }
+            set { tipoCuenta = value; }
+        }
+
+        public float Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public int NumeroCuenta 
+        {
+            get { return numeroCuenta; }
+        }
+        
+        //Constructor de la clase
+        public CuentaBancaria()
+        {
+            //Por medio de la clase random se genera el número de cuenta
+            Random rnd = new Random();
+            numeroCuenta = rnd.Next(10000000, 99999999);
+        }
+        
+        //Métodos
+        //Retirar Dinero
+        public void retirarDinero(float valorRetiro)
+        {
+            if (valorRetiro > this.saldo)
+            {
+                Console.WriteLine("Error, El saldo de la cuenta es insuficiente.");
+            }
+            else
+            {
+                this.saldo -= valorRetiro;
+                Console.WriteLine("El saldo de la cuenta después del retiro es: " + saldo + "$");
+            }
+        }
+        //Consignar Dinero
+        public void consignarDinero(float valorConsig)
+        {
+            if (valorConsig > 0)
+            {
+                this.saldo += valorConsig;
+                Console.WriteLine("El saldo de la cuenta después de la consignación es: " + saldo + "$");
+            }
+            else
+            {
+                Console.WriteLine("Error, entrada inválida");
+            }
+        }
+        //Ver Saldo Actual
+        public void verSaldo()
+        {
+            Console.WriteLine("El saldo actual de la cuenta es: " + saldo + "$");
+        }
+        
+        //Ver Todos los datos de la cuenta
+        public void verTodosDatos()
+        {
+            Console.WriteLine("Los datos de la cuenta son: \n" +
+                              "Titular: {0} \n" +
+                              "Tipo de Cuenta: {1} \n" +
+                              "Número de Cuenta: {2} \n" +
+                              "Saldo: {3}$ \n", nombreTitular,tipoCuenta,numeroCuenta,saldo);         
+        }
+    }
+}
---
+++ Segundo Corte/Laboratorio POO 1/LabPOO/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace LabPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreTitular; bool listo = false;
+            CuentaBancaria cuentaActual = new CuentaBancaria();
+            
+            Console.WriteLine("Bienvenido a Bancos La Gloria Carmen, Porfavor digite la información solicitada para crear su cuenta.\n" +
+                              "Ingrese su nombre completo: ");
+            nombreTitular = Console.ReadLine();
+            cuentaActual.NombreTitular = nombreTitular;
+            
+            while(true)
+            {
+                Console.WriteLine("\nIngrese el tipo de cuenta, digite el número de la opción (1,2):  \n" +
+                                  "1. Ahorros. \n" +
+                                  "2. Corriente.");
+                
+                var opcion = Console.ReadLine();
+                if (opcion == "1")
+                {
+                    cuentaActual.TipoCuenta = "Ahorros";
+                    cuentaActual.Saldo = 50000;
+                    listo = true;
+                    break;
+                }
+                else if (opcion == "2")
+                {
+                    cuentaActual.TipoCuenta = "Corriente";
+                    cuentaActual.Saldo = 100000;
+                    listo = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error, la selección no se encuentra dentro del menú de opciones.");
+                }
+            }
+            Console.WriteLine("\nLa cuenta ha sido creada satisfactoriamente.\nTitular: {0}, Tipo de Cuenta: {1}, Numero de la cuenta: {2}.",cuentaActual.NombreTitular,cuentaActual.TipoCuenta,cuentaActual.NumeroCuenta);
+            
+            //Menu Opciones Cuenta
+            while(listo)
+            {
+                Console.WriteLine("\nDigite el número de la opción que desee seleccionar: \n" +
+                                  "1.Retirar dinero \n" +
+                                  "2.Consignar dinero \n" +
+                                  "3.Ver saldo actual \n" +
+                                  "4.Ver todos los detalles de la cuenta \n" +
+                                  "5.Salir");
+                
+                var menuOpcion = Console.ReadLine();
+                switch (menuOpcion)
+                {
+                    case "1": 
+                        Console.WriteLine("\nIngrese la cantidad de dinero que quiere retirar, debe ser menor a: " + cuentaActual.Saldo + "$");
+                        var retiro = float.Parse(Console.ReadLine());
+                        
+                        cuentaActual.retirarDinero(retiro);
+                        break;
+                    
+                    case "2": 
+                        Console.WriteLine("\nIngrese la cantidad de dinero que quiere consignar: ");
+                        var consig = float.Parse(Console.ReadLine());
+                        
+                        cuentaActual.consignarDinero(consig);
+                        break;
+                    
+                    case "3":
+                        cuentaActual.verSaldo();
+                        break;
+                    
+                    case "4":
+                        cuentaActual.verTodosDatos();
+                        break;
+                    
+                    case "5":
+                        Console.WriteLine("\nGracias por usar nuestros servicios.");
+                        listo = false;
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
