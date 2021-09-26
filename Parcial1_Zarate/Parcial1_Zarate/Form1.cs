4c0579c8e85b104f53ee72a630cb6a3786ccfcf7
Sebastian Zarate <63659954+redclazz2@users.noreply.github.com>
2021-09-25 23:52:22 -0500

    Parcial

---
+++ Parcial1_Zarate/Parcial1_Zarate/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1_Zarate
+{
+    public partial class Form1 : Form
+    {
+        public void cargarFormulario(string tipoUsuario) 
+        {
+            Form formCarga;
+            if(tipoUsuario == "Admin") { formCarga = new Form2(this); }
+            else { formCarga = new Form3(this); }
+
+            this.Hide();
+            formCarga.Show();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if ((txtUsuario.Text == "admin") && (txtContra.Text == "123"))
+            {
+                cargarFormulario("Admin");
+            }
+            else if (((txtUsuario.Text == "empleado") && (txtContra.Text == "321"))) 
+            {
+                cargarFormulario("Usuario");
+            }
+            else 
+            {
+                MessageBox.Show("Usuario o Contrasena Incorrectos ... ");
+            }
+
+        }
+    }
+}
---
+++ Parcial1_Zarate/Parcial1_Zarate/Form3.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Parcial1_Zarate
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nmPanAlinado = new System.Windows.Forms.NumericUpDown();
+            this.nmPanNoAlinado = new System.Windows.Forms.NumericUpDown();
+            this.nmEspeciales = new System.Windows.Forms.NumericUpDown();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nmPanAlinado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmPanNoAlinado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmEspeciales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cerrar Sesión";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "---------------------------------------------------------------------------------" +
+    "----------------";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Panel de Empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OnlyPans";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(20, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 24);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Generar Factura";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cantidad de Productos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Pan Aliñado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Pan No Aliñado";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Especiales";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 216);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Nombre del Cliente";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Cédula";
+            // 
+            // nmPanAlinado
+            // 
+            this.nmPanAlinado.Location = new System.Drawing.Point(90, 107);
+            this.nmPanAlinado.Name = "nmPanAlinado";
+            this.nmPanAlinado.Size = new System.Drawing.Size(113, 20);
+            this.nmPanAlinado.TabIndex = 14;
+            // 
+            // nmPanNoAlinado
+            // 
+            this.nmPanNoAlinado.Location = new System.Drawing.Point(107, 143);
+            this.nmPanNoAlinado.Name = "nmPanNoAlinado";
+            this.nmPanNoAlinado.Size = new System.Drawing.Size(113, 20);
+            this.nmPanNoAlinado.TabIndex = 15;
+            // 
+            // nmEspeciales
+            // 
+            this.nmEspeciales.Location = new System.Drawing.Point(84, 175);
+            this.nmEspeciales.Name = "nmEspeciales";
+            this.nmEspeciales.Size = new System.Drawing.Size(113, 20);
+            this.nmEspeciales.TabIndex = 16;
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(122, 213);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(140, 20);
+            this.txtNombreCliente.TabIndex = 17;
+            this.txtNombreCliente.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombreCliente_KeyPress);
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(66, 246);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(140, 20);
+            this.txtCedula.TabIndex = 18;
+            this.txtCedula.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCedula_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label10.Location = new System.Drawing.Point(322, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(212, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Aquí se mostrará la última factura generada";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(322, 109);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(128, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Última Compra Realizada:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(20, 282);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(359, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Nota: La fecha y hora son determinadas automáticamente por el programa.";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 362);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.nmEspeciales);
+            this.Controls.Add(this.nmPanNoAlinado);
+            this.Controls.Add(this.nmPanAlinado);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.nmPanAlinado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmPanNoAlinado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmEspeciales)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown nmPanAlinado;
+        private System.Windows.Forms.NumericUpDown nmPanNoAlinado;
+        private System.Windows.Forms.NumericUpDown nmEspeciales;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
